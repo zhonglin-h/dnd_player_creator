f076164ed950a778021439119e63016f123410d9
zhonglin-h <51329642+jjpdn@users.noreply.github.com>
2021-05-06 21:04:11 -0400

    added to about tab

--- right_page/right_page_main.js
+++ right_page/right_page_main.js
@@ -5,15 +5,13 @@ let current_tab = 'about-tab'
 
 function change_tab (new_tab_id) {
 
-    ipcRenderer.send('debug', 'changed')
-
     // make current tab invisible
     let old_tab = document.getElementById(current_tab)
     old_tab.style.display = 'none'
 
     // make new tab visible
     let new_tab = document.getElementById(new_tab_id)
-    new_tab.style.display = 'block'
+    new_tab.style.display = 'flex'
     
     current_tab = new_tab_id
 }
@@ -22,14 +20,10 @@ function set_click_listeners() {
     const right_about_tab_button = document.getElementById('right-about-tab-button')
     const right_spells_tab_button = document.getElementById('right-spells-tab-button')
 
-    ipcRenderer.send('debug', 'changed')
-
     right_about_tab_button.addEventListener('click', function() {
-        ipcRenderer.send('debug', 'changed')
         change_tab('about-tab')
     })
     right_spells_tab_button.addEventListener('click', function() {
-        ipcRenderer.send('debug', 'changed')
         change_tab('spells-tab')
     })
 }
